cd8cfaec35150cc2b11c51e3efa3d5b3420b629e
Pierre Fernbach <pierre.fernbach@gmail.com>
2020-08-06 15:47:07 +0200

    Update reference end effector position in ROM class

--- talos_rbprm/talos_abstract.py
+++ talos_rbprm/talos_abstract.py
@@ -27,13 +27,16 @@ class Robot(Parent):
 
     ref_height = 1.0225
     # reference position of the end effector position for each ROM
-
-    ref_EE_lLeg = [-0.008846952891378526, 0.0848172440888579, -1.0225]
+    ref_EE_lLeg = [-0.008846952891378526, 0.0848172440888579, -1.01977]
     ref_EE_lLeg[0] = 0.  # assure symetry of dynamic constraints on flat ground
-    ref_EE_rLeg = [-0.008846952891378526, -0.0848172440888579, -1.0225]
+    ref_EE_rLeg = [-0.008846952891378526, -0.0848172440888579, -1.01977]
     ref_EE_rLeg[0] = 0.
     ref_EE_lArm = [0.13028765672452458, 0.44360498616312666, -0.2881211563246389]
     ref_EE_rArm = [0.13028765672452458, -0.44360498616312666, -0.2881211563246389]
+    dict_ref_effector_from_root = {rLegId:ref_EE_rLeg, # Effector position in the reference configuration, in the root frame
+                                   lLegId:ref_EE_lLeg,
+                                   rArmId:ref_EE_rArm,
+                                   lArmId:ref_EE_lArm}
 
     def __init__(self, name=None, load=True, client=None, clientRbprm=None):
         if name is not None:
